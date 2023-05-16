030b54e320468dccf28a1743d7810c4519633886
DanoCH-boop <danochudy@DESKTOP-379DKVR.localdomain>
2023-05-16 14:12:07 +0200

    Removing old implementation folder

--- src_without/misc/time_convert.py
+++
@@ -1,4 +0,0 @@
-
-def time_convert(time):
-    """Convets time from time format to seconds"""
-    return time.hours*3600 +time.minutes*60 + time.seconds + time.milliseconds/1000
--- src_without/startup.py
+++
@@ -1,7 +0,0 @@
-from frontend.help import App
-import sys
-
-app = App()
-app.mainloop()
-# kill all daemon threads if align goes wrong
-sys.exit()
